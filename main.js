33418c3465df65b760f5643411a7f0871c125feb
Sebasorova <56850980+SebastianSoftware@users.noreply.github.com>
2023-07-21 21:38:25 -0500

    v0.3 (Dev)

--- main.js
+++ main.js
@@ -1,15 +1,45 @@
 //Variables
-let color=13; //Colores en 8 bits
+let precolor = prompt('Escribe un color'); //Prompt para seleccionar el color para dibujar
+//Mapeado de los colores (actualmente 16 colores)
+const colores = `{
+ 'rojo': 1,
+ 'naranja': 2,
+ 'amarillo': 3,
+ 'verde oscuro': 4,
+ 'verde claro': 5,
+ 'azul oscuro': 6,
+ 'azul': 7,
+ 'azul claro': 8,
+ 'purpura oscuro': 9,
+ 'morado oscuro': 9,
+ 'purpura': 10,
+ 'morado': 10,
+ 'rosa claro': 11,
+ 'rosa': 11,
+ 'marrón': 12,
+ 'marron': 12,
+ 'café': 12,
+ 'cafe': 12,
+ 'negro': 13,
+ 'gris': 14,
+ 'gris claro': 15,
+ 'blanco': 16
+}`;
+let colortradc0 = JSON.parse(colores);
+let colortradc1 = precolor.toLowerCase();
+let color = colortraduc0[colortradc1];
 //Coordenadas X (Horizontal)
-let xmin =750; // Minimo 0
-let xmax =999; // Máximo 999 (tamaño actual máximo del mapa)
+let xmin = prompt('Escribe la coordenada X (horizontal) mínima para dibujar'); // Minimo 0
+let xmax = prompt('Escribe la coordenada X (horizontal) máxima para dibujar'); // Máximo 999 (tamaño actual máximo del mapa)
 //Coordenadas Y (Vertical)
-let ymin =750; // Mínimo 0
-let ymax =999; // Máximo 999
+let ymin = prompt('Escribe la coordenada Y (vertical) mínima para dibujar'); // Mínimo 0
+let ymax = prompt('Escribe la coordenada Y (vertical) máxima para dibujar'); // Máximo 999
  
 let modhash;
- 
-function sleep (delay) {
+
+if(color !=undefined || color > 16) {
+ if(xmin > 0 && xmax < 999 && ymin > 0 && xmax < 499) {
+  function sleep (delay) {
   return new Promise(resolve => setTimeout(resolve, delay));
 }
  
@@ -18,7 +48,7 @@ async function drawWhenReady() {
     console.log("Error: No se pudo dibujar, por esperando 15 segundos más.");
     await sleep(15000);
   }
-  console.log("We can draw now!");
+  console.log("DEBUG: ya se puede dibujar.");
   drawRandomPixel();
 }
  
@@ -45,3 +75,11 @@ function init() {
 }
  
 setTimeout(init, 1500);
+ }
+ else {
+  console.log("ERROR: coordenadas inválidas.");
+ }
+} else {
+ console.log("ERROR: color inválido.");
+}
+
